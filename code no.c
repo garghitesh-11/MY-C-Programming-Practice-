ae077e4e83c9930ada018e717568e31953ee0063
garghitesh-11 <garghitesh122@gmail.com>
2026-03-04 00:31:43 +0530

    table of any number n

--- code no.c
+++ code no.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int i;
-
-    printf("we will write the table of 6=");
-    for (i = 0; i < 20; i++)
+    int i, n, a;
+    printf("enter the number =\n");
+    scanf("%d", &n);
+    printf("we will write the table of %d\n", n);
+    for (i = 0; i < 21; i++)
     {
-
-        printf("%d\n", 6 * i);
+        a = n * i;
+        printf("%d*%d=%d\n", n, i, a);
     }
     return 0;
 }
