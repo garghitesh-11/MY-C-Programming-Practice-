ae7058d319ee6f80cdbbb9fbb2e15e8f7c6c38ea
garghitesh-11 <garghitesh122@gmail.com>
2026-03-03 16:56:01 +0530

    initial programs of c

---
+++ code no.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+
+    printf("we will write the table of 6=");
+    for (i = 0; i < 20; i++)
+    {
+
+        printf("%d\n", 6 * i);
+    }
+    return 0;
+}
